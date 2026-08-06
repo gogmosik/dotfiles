461aa775a9b0504613f59982139a4656a768a20b
gogmosik <gogmosik@gmail.com>
2026-08-06 20:01:13 +0300

    hyprland config GLOBAL fixes

--- .config/hypr/hyprland.lua
+++ .config/hypr/hyprland.lua
@@ -187,11 +187,32 @@ hl.config({
 
 hl.config({
     animations = {
-        enabled = { true, "please:)" },
-        -- Default curves, see https://wiki.hypr.land/Configuring/Animations/#curves
-        --        NAME,           X0,   Y0,   X1,   Y1
-        -- Default animations, see https://wiki.hypr.land/Configuring/Animations/
-        --           NAME,          ONOFF, SPEED, CURVE,        [STYLE]
+        enabled = true,
+        
+        hl.curve("easeOutQuint", {type="bezier", points={{0.23, 1}, {0.32, 1}}}),
+        hl.curve("easeInOutCubic", {type="bezier", points={{0.65, 0.05}, {0.36, 1}}}),
+        hl.curve("linear", {type="bezier", points={{0, 0}, {1, 1}}}),
+        hl.curve("almostLinear", {type="bezier", points={{0.5, 0.5}, {0.75, 1}}}),
+        hl.curve("quick", {type="bezier", points={{0.15, 0}, {0.1, 1}}}),
+        hl.curve("easeOut", {type="bezier", points={{0.22, 1}, {0.36, 1}}}),
+        hl.curve("easeIn", {type="bezier", points={{0.36, 0}, {0.66, -0.56}}}),
+
+        hl.animation({leaf= "global", enabled=true, speed=10, bezier="default"}),
+        hl.animation({leaf= "windows", enabled=true, speed=7, bezier="easeOut", style="slide"}),
+        hl.animation({leaf= "windowsIn", enabled=true, speed=5, bezier="easeOut", style="popin 80%"}),
+        hl.animation({leaf= "windowsOut", enabled=true, speed=6, bezier="easeIn", style="popin 80%"}),
+        hl.animation({leaf= "fadeIn", enabled=true, speed=1.73, bezier="almostLinear"}),
+        hl.animation({leaf= "fadeOut", enabled=true, speed=1.46, bezier="almostLinear"}),
+        hl.animation({leaf= "fade", enabled=true, speed=8, bezier="easeOut"}),
+        hl.animation({leaf= "layers", enabled=true, speed=3.81, bezier="easeOutQuint"}),
+        hl.animation({leaf= "layersIn", enabled=true, speed=4, bezier="easeOutQuint", style="fade"}),
+        hl.animation({leaf= "layersOut", enabled=true, speed=1.5, bezier="linear", style="fade"}),
+        hl.animation({leaf= "fadeLayersIn", enabled=true, speed=1.79, bezier="almostLinear"}),
+        hl.animation({leaf= "fadeLayersOut", enabled=true, speed=1.39, bezier="almostLinear"}),
+        hl.animation({leaf= "workspaces", enabled=true, speed=6, bezier="easeOut", style="slide"}),
+        hl.animation({leaf= "workspacesIn", enabled=true, speed=3, bezier="easeOut", style="slidefade"}),
+        hl.animation({leaf= "workspacesOut", enabled=true, speed=2, bezier="easeIn", style="slidefade"}),
+        hl.animation({leaf= "zoomFactor", enabled=true, speed=7, bezier="quick"}),
     },
 })
 
@@ -258,7 +279,7 @@ hl.config({
 
 hl.config({
     input = {
-        kb_layout = { "us", "ru" },
+        kb_layout = "us,ru",
         kb_options = "grp:alt_shift_toggle",
         follow_mouse = 1,
         accel_profile = "flat",
@@ -306,7 +327,7 @@ hl.bind(mainMod .. " + " .. "TAB", hl.dsp.exec_cmd("rofi -show drun"))
 
 --bind = $mainMod, J, togglesplit, # dwindle
 
-hl.bind("$mainMOD + SHIFT + CONTROL" .. " + " .. "S", hl.dsp.exec_cmd("kill hyprpicker"))
+hl.bind("SUPER + SHIFT + CONTROL" .. " + " .. "S", hl.dsp.exec_cmd("kill hyprpicker"))
 hl.bind(mainMod .. " + " .. "SHIFT" .. " + " .. "S", hl.dsp.exec_cmd("hyprshot -z -m region --clipboard-only"))
 hl.bind(mainMod .. " + " .. "SHIFT" .. " + " .. "D", hl.dsp.exec_cmd("hyprshot -z -m output --clipboard-only"))
 hl.bind(mainMod .. " + " .. "SHIFT" .. " + " .. "A", hl.dsp.exec_cmd("com.ayugram.desktop"))
